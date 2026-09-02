53f8c58324aa08126c47d44e726117c0a89c4b0f
Justin Venable <dartsolarpunk@gmail.com>
2026-09-02 15:13:29 -0400

    Fix: remove duplicate ShareSheet (already in IDEMainView)

--- Sources/AshtreeIDEApp/Views/IDEMindMapView.swift
+++ Sources/AshtreeIDEApp/Views/IDEMindMapView.swift
@@ -1323,11 +1323,4 @@ struct MashExportSheet: View {
     }
 }
 
-// ShareSheet wrapper
-struct ShareSheet: UIViewControllerRepresentable {
-    let items: [Any]
-    func makeUIViewController(context: Context) -> UIActivityViewController {
-        UIActivityViewController(activityItems: items, applicationActivities: nil)
-    }
-    func updateUIViewController(_: UIActivityViewController, context: Context) {}
-}
+// ShareSheet is defined in IDEMainView.swift
