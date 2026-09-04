b10b037f2ce646a6d970293c7c01d86cf87ceb92
Justin Venable <dartsolarpunk@gmail.com>
2026-09-04 03:51:42 -0400

    Merge: ASH node types, code gen, forward-compat migration from 510

--- Sources/AshtreeIDEApp/Views/IDEMindMapModel.swift
+++ Sources/AshtreeIDEApp/Views/IDEMindMapModel.swift
@@ -18,15 +18,6 @@ public enum MashNodeType: String, Codable, CaseIterable {
     case note       = "note"        // Information leaf
     case image      = "image"       // Image node
     case link       = "link"        // Hyperlink node
-    // ASH coding template node types
-    case ashCode    = "ash_code"    // ASH syntax code block
-    case outTerminal = "out_terminal" // Terminal/CLI output
-    case out2D      = "out_2d"      // 2D canvas / vector output
-    case out3D      = "out_3d"      // 3D scene / WebGL output
-    case inputForm  = "input_form"  // User input form node
-    case outputForm = "output_form" // Output display form node
-    case returnNode = "return"      // Return/result node
-
 
     public var defaultRadius: CGFloat {
         switch self {
@@ -649,8 +640,6 @@ public struct MashAshCodeGenerator {
                 }
             case .image:
                 lines.append("\(indent)// [image node: \(text)]")
-            default:
-                lines.append("\(indent)// \(text)")
             }
 
             // Process children
